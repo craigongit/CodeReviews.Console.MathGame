d3b4ba75605380ea3aca363a49cd63726eb0494d
craigongit <nbenicraig@gmail.com>
2025-11-04 18:44:32 +0200

    Implement basic Addition operation in Operations class

---
+++ MathGame.craigongit/Operations.cs
@@ -0,0 +1,24 @@
+using Spectre.Console;
+
+namespace MathGame;
+
+public class Operations
+{
+    private List<Game> _gameHistory = [];
+    private readonly Helpers _helpers = new Helpers();
+
+    public void Addition()
+    {
+        var operands = _helpers.GenerateOperands();
+        var correctAnswer = operands[0] + operands[1];
+        
+        _helpers.DisplayMessage("Solve this operation:");
+        
+        var userAnswer = AnsiConsole.Ask<int>($"[white]{operands[0]} + {operands[1]}[/]");
+        
+        _helpers.VerifyAnswer(userAnswer, correctAnswer);
+        
+        // Save game
+        var game = new Game("Addition", operands[0], operands[1]);
+    }
+}

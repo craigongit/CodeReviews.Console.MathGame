dfc36d6482b9f3762b0d7150378bfe3122728024
craigongit <nbenicraig@gmail.com>
2025-11-04 17:38:28 +0200

    Make basic app flow

--- MathGame.craigongit/Program.cs
+++ MathGame.craigongit/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MathGame;
+using Spectre.Console;
 
-Console.WriteLine("Hello, World!");
+while (true)
+{
+    var userChoice = AnsiConsole.Prompt(
+        new SelectionPrompt<UserOptions>()
+            .Title("Select an option!")
+            .AddChoices(Enum.GetValues<UserOptions>()));
+
+    switch (userChoice)
+    {
+        case UserOptions.Addition:
+            // Operations.Addition()
+            break;
+        case UserOptions.Subtraction:
+            // Operations.Subtraction()
+            break;
+        case UserOptions.Multiplication:
+            // Operations.Multiplication()
+            break;
+        case UserOptions.Division:
+            // Operations.Division()
+            break;
+        case UserOptions.History:
+            // Operations.History()
+            break;
+        case UserOptions.Quit:
+            // Operations.Quit()
+            break;
+    }
+}
